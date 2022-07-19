92973bc8fd7e0529d941fb945c6cd83f12454019
undef125 <076bct038@ioepc.edu.np>
2022-07-19 19:50:05 +0545

    solving cors problem

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ const bodyParser = require("body-parser");
 
 //middlewares
 
-const allowList = ["https://booksmartnepal.netlify.app"]
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Log and check yourself if the origin actually matches what you've defined in the allowList array
-    console.log(origin);
+// const allowList = ["https://booksmartnepal.netlify.app"]
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     // Log and check yourself if the origin actually matches what you've defined in the allowList array
+//     console.log(origin);
 
-    if (allowList.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  credentials: true,
-}
+//     if (allowList.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   },
+//   credentials: true,
+// }
 
 // var corsOptions = {
 //   origin: 'https://booksmartnepal.netlify.app',
@@ -30,7 +30,17 @@ const corsOptions = {
 //   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 // }
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use(
+  cors({
+    origin: ["https://upbeat-northcutt-4650a4.netlify.app"],
+    methods: ["GET", "POST", "DELETE"],
+    credentials: true,
+    origin: true,
+  })
+);
+
 app.use(express.json());
 app.use("/", router);
 app.use(bodyParser.json({extended: true}));
