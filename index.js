513198410b29bc9ff5390d4f8b50cc2af8a541dd
undef125 <076bct038@ioepc.edu.np>
2022-07-19 18:55:26 +0545

    solving cors problem

--- index.js
+++ index.js
@@ -7,17 +7,24 @@ const bodyParser = require("body-parser");
 
 //middlewares
 
-const whitelist = ["http://localhost:3000", "https://booksmartnepal.netlify.app"]
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
+// const whitelist = ["http://localhost:3000", "https://booksmartnepal.netlify.app"]
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error("Not allowed by CORS"))
+//     }
+//   },
+//   credentials: true,
+// }
+
+var corsOptions = {
+  origin: 'https://booksmartnepal.netlify.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
+
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/", router);
