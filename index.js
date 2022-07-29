d9289f22cee302269c0e1ad0a8cf3065640caa72
undef125 <076bct038@ioepc.edu.np>
2022-07-29 15:50:32 +0545

    socketiointegrated

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ let db = connectDB();
 //middlewares
 app.use(
   cors({
-    // origin: ["https://booksmartnepal.netlify.app", "https://localhost:3000"],
-    origin: ["http://localhost:3000"],
+    origin: ["https://booksmartnepal.netlify.app"],                   //production
+    //origin: ["http://localhost:3000"],                                    //development
     methods: ["GET", "POST", "DELETE"],
     credentials: true,
     origin: true,
