3bdc4861e6094101fca6317e19db3fd33c6a6f0c
undef125 <076bct038@ioepc.edu.np>
2022-07-29 15:49:18 +0545

    socketiointegrated

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 const express = require("express");
 const app = express();
-const connectDB = require("./database/Database");           //database connection
+const connectDB = require("./database/Database"); //database connection
 const router = require("./routes/routes");
-const cors = require('cors');
+const cors = require("cors");
 const bodyParser = require("body-parser");
+const { Server } = require("socket.io");
+const http = require("http");
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "DELETE"],
+    credentials: true,
+    origin: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("join_room", (chatid) => {
+    socket.join(chatid);
+    console.log(`socketid: ${socket.id} and chat id: ${chatid}`);
+  });
+
+  socket.on("send_message", (message) => {
+    socket.to(message.chatId).emit("receive_message", message);
+    console.log(message);
+  })
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected: ", socket.id);
+  });
+});
 
 //connecting to database
 let db = connectDB();
@@ -11,7 +42,8 @@ let db = connectDB();
 //middlewares
 app.use(
   cors({
-    origin: ["https://booksmartnepal.netlify.app"],
+    // origin: ["https://booksmartnepal.netlify.app", "https://localhost:3000"],
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST", "DELETE"],
     credentials: true,
     origin: true,
@@ -20,9 +52,11 @@ app.use(
 
 app.use(express.json());
 app.use("/", router);
-app.use(bodyParser.json({extended: true}));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use('/uploads', express.static('./uploads')); //to use the images url from upload folder
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/uploads", express.static("./uploads")); //to use the images url from upload folder
 
 //listening port
-app.listen(process.env.PORT || 5000);
+// app.listen(process.env.PORT || 5000);
+
+server.listen(5000);
