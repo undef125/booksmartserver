72336735bd98e236c9ad13e90639cb93ff1cdffc
undef125 <076bct038@ioepc.edu.np>
2022-08-17 06:33:30 +0545

    finilazing phase 1

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     // origin: ["http://localhost:3000"],                         //development
-    origin: ["https://booksmartnepal.netlify.app/"],          //production
+    origin: ["https://booksmartnepal.netlify.app"],          //production
     methods: ["PUT","GET", "POST", "DELETE"],
     credentials: true,
     origin: true,
