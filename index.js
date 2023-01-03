1764cb76e723837d08fcb2ab2364dcb75784d6c0
undef125 <076bct038@ioepc.edu.np>
2023-01-03 17:32:21 +0545

    ..

--- index.js
+++ index.js
@@ -7,35 +7,35 @@ const bodyParser = require("body-parser");
 const { Server } = require("socket.io");
 const http = require("http");
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    // origin: ["http://localhost:3000"],                         //development
-    origin: ["https://booksmartnepal.netlify.app"],          //production
-    methods: ["PUT","GET", "POST", "DELETE"],
-    credentials: true,
-    origin: true,
-  },
-});
+// const io = new Server(server, {
+//   cors: {
+//     // origin: ["http://localhost:3000"],                         //development
+//     origin: ["https://booksmartnepal.netlify.app"],          //production
+//     methods: ["PUT","GET", "POST", "DELETE"],
+//     credentials: true,
+//     origin: true,
+//   },
+// });
 
-io.on("connection", (socket) => {
-  console.log(socket.id);
+// io.on("connection", (socket) => {
+//   console.log(socket.id);
 
-  socket.on("join_room", (chatid) => {
-    socket.join(chatid);
-    // console.log(`socketid: ${socket.id} and chat id: ${chatid}`);
-  });
+//   socket.on("join_room", (chatid) => {
+//     socket.join(chatid);
+//     // console.log(`socketid: ${socket.id} and chat id: ${chatid}`);
+//   });
 
-  socket.on("send_message", (message) => {
-    socket.to(message.chatId).emit("receive_message", message);
-    // console.log(message);
-  })
+//   socket.on("send_message", (message) => {
+//     socket.to(message.chatId).emit("receive_message", message);
+//     // console.log(message);
+//   })
 
-  socket.on("disconnect", () => {
-    // console.log("User Disconnected: ", socket.id);
-  });
-});
+//   socket.on("disconnect", () => {
+//     // console.log("User Disconnected: ", socket.id);
+//   });
+// });
 
 //connecting to database
 let db = connectDB();
@@ -60,4 +60,5 @@ app.use("/uploads", express.static("./uploads")); //to use the images url from u
 //listening port
 // app.listen(process.env.PORT || 5000);        //production
 
-server.listen(process.env.PORT || 5000);                            //development
+// server.listen(process.env.PORT || 5000);                            //development
+app.listen(process.env.PORT || 5000);                            //development
